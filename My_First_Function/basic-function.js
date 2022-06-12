93efc120701fca5dc42032eee3142d161132400d
Sebastian <89323242+sebastianelbacho@users.noreply.github.com>
2022-06-12 14:28:24 +0100

    Update basic-function.js

--- My_First_Function/basic-function.js
+++ My_First_Function/basic-function.js
@@ -1,5 +1,5 @@
-function My_First_Function() {
-    var str = "This text is green!";
-    var result = str.fontcolor("green");
-    document.getElementById("Green_Text").innerHTML = result;
+function My_First_Function() {                  //Defining a function and namimg it 
+    var str = "This text is green!";        //Defining a variable and giving a string value
+    var result = str.fontcolor("green");    //Using  the fontcolor method on str variable
+    document.getElementById("Green_Text").innerHTML = result; // Puttin the value of result into HTML element with "Green_Text" ID 
 }
