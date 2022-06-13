51238c44acf06ed8fd1951d8208df0de85931a05
Sebastian <89323242+sebastianelbacho@users.noreply.github.com>
2022-06-13 09:48:44 +0100

    Create script.js

---
+++ Assignment 1/script.js
@@ -0,0 +1,17 @@
+window.alert("Welcome! I'm still learning so, please understanding me :)"); //shows the text in a window
+window.alert("Hello Visitor!"); // display the text after window.alert
+
+
+var logo = document.querySelector('.container'); //variable and function 
+var menu = document.querySelector('.menu');
+
+logo.addEventListener('click', function () {
+    menu.classList.toggle('showmenu');
+
+})
+
+let text = "and all mankind is on one sea";
+var result = text.fontcolor("yellow");
+document.getElementById("yellow").innerHTML = result;
+
+document.write("\"I’m not in this world to live up to your expectations and you’re not in this world to live up to mine.\"" + "-Bruce Lee");
