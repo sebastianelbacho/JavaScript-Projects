dd7ef40ded18fe5c953ff5aab65fcfa7515434cf
Sebastian <89323242+sebastianelbacho@users.noreply.github.com>
2022-06-26 17:04:13 +0100

    Create script.js

---
+++ Assignment 3.1/script.js
@@ -0,0 +1,7 @@
+function myFunction() {  // Named of function
+    var addition = 243 + 489 - 78 * 4; // variable 
+    document.getElementById("assignment").innerHTML = "243 + 489 - 78 * 4 = " + addition; // select operation from the text
+
+}
+
+window.alert(Math.random() % 0.001); //display random number in window block alert 
