b8486195973e207fff969ba97b6182a78be6eede
Sebastian <89323242+sebastianelbacho@users.noreply.github.com>
2022-06-26 19:17:17 +0100

    Update main.js

--- Project4_dictionaries/main.js
+++ Project4_dictionaries/main.js
@@ -1,1 +1,11 @@
-
+function my_Dictionary() {
+    var Guitar = {
+        kind: "6 strings guitar",
+        type: "classic-acustic",
+        color: "amber-yellow",
+        wood: "ash",
+        sound: "hard and clear",
+        frets: "24",
+    }
+    document.getElementById("dictionary").innerHTML = Guitar.color;
+}
