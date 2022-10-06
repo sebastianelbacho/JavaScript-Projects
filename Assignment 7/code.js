bfcf6c83a55a237d36d37d64ac5e62887388c8e1
Sebastian <89323242+sebastianelbacho@users.noreply.github.com>
2022-10-06 13:59:10 +0100

    Update code.js

--- Assignment 7/code.js
+++ Assignment 7/code.js
@@ -1,24 +1,27 @@
-var today = new Date();
-var hourNow = today.getHours();
-var greetings;
+var today = new Date();    // variable 
+var hourNow = today.getHours();   // variablem takes the exact hour
+var greetings;     
 
-if (hourNow < 12) {
+if (hourNow < 12) {     // the variable display good morning info before 12 o'clock
     greetings = "Good Morning!";
-} else if (hourNow > 12) {
-    greetings = "Good Aftrnoon!";
-} else if (hourNow > 18) {
+} else if (hourNow > 12) {      // the variable display good afernoon info after 12 o'clock
+    greetings = "Good Aftrnoon!";  
+} else if (hourNow > 18) {     // the variable dispaly good evening after 18 o'clock
     greetings = "Good Evening!";
 } else {
-    greetings = "Welcome!";
+    greetings = "Welcome!";    // 
 }
 
-document.write("<h4>" + greetings + "</h4>");
+document.write("<h4>" + greetings + "</h4>"); //the program is attached under <h4> word
 
 
-function getArea(width, height) {
+function getArea(width, height) {   //Local function
     var area = width * height;
     return area;
 }
 
-let wallSize = getArea(4, 7);
+let wallSize = getArea(4, 7);  //global function
 document.write(wallSize);
+
+
+console.error("");
