2f665cc686730b1fe87813ef3061af8c2cc1ec68
Sebastian <89323242+sebastianelbacho@users.noreply.github.com>
2022-10-09 17:58:13 +0100

    Create code.js

---
+++ concat() Method/code.js
@@ -0,0 +1,13 @@
+
+
+
+function full_Sentence() {
+    var part_1 = "I have ";
+    var part_2 = "made this ";
+    var part_3 = "into a complete ";
+    var part_4 = "sentence. ";
+    var whole_sentence = part_1.concat(part_2, part_3, part_4);
+    document.getElementById("Concatenate").innerHTML = whole_sentence;
+
+
+}
