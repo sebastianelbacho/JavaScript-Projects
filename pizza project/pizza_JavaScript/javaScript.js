5257d835e672ae5f439ee52cc7ec97537a1600d9
Sebastian <89323242+sebastianelbacho@users.noreply.github.com>
2022-12-16 14:08:14 +0000

    Update javaScript.js

--- pizza project/pizza_JavaScript/javaScript.js
+++ pizza project/pizza_JavaScript/javaScript.js
@@ -1,1 +1,14 @@
-
+function getReceipt() {
+    // This initializes our string so it can get passed from
+    // function to function, growning line by line into a full receipt
+    var text1 = "<h3>You Ordered:</h3>";
+    var runningTotal = 0;
+    var sizeTotl = 0;
+    var sizeArray = document.getElementsByClassName("size");
+    for (var i = 0; i < sizeArray.length; i++) {
+        if (sizeArray[i].checked) {
+            var selectedSize = sizeArray[i].value;
+            text1 = text1 + selectedSize + "<br>";
+        }
+    }
+}
