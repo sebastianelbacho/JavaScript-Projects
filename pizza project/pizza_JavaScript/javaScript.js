8ae1f9eaea84abca0b444486c3ec7d1d64e7f49f
Sebastian <89323242+sebastianelbacho@users.noreply.github.com>
2022-12-24 21:52:59 +0000

    Update javaScript.js

--- pizza project/pizza_JavaScript/javaScript.js
+++ pizza project/pizza_JavaScript/javaScript.js
@@ -3,7 +3,7 @@ function getReceipt() {
     // function to function, growning line by line into a full receipt
     var text1 = "<h3>You Ordered:</h3>";
     var runningTotal = 0;
-    var sizeTotl = 0;
+    var sizeTotal = 0;
     var sizeArray = document.getElementsByClassName("size");
     for (var i = 0; i < sizeArray.length; i++) {
         if (sizeArray[i].checked) {
@@ -11,4 +11,22 @@ function getReceipt() {
             text1 = text1 + selectedSize + "<br>";
         }
     }
-}
+    if (selectedSize === "Personal Pizza") {
+        sizeTotal = 6;
+    } else if (selectedSize === "Small Pizza") {
+        sizeTotal = 8;
+    } else if (selectedSize === "Medium Pizza") {
+        sizeTotal = 10;
+    } else if (selectedSize === "Large Pizza") {
+        sizeTotal = 14;
+    } else if (selectedSize === "Extra Large Pizza") {
+        sizeTotal = 16;
+    }
+
+    runningTotal = sizeTotal;
+    console.log(selectedSize + " = $" + sizeTotal + ".00");
+    console.log("size text1: " + text1);
+    console.log("subtotal: $" + runningTotal + ".00");
+    // these variables will get passed on to each function
+    getTopping(runningTotal, text1);
+};
