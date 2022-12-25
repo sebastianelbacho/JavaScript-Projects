b5e0c17948e115ade32040223f08a99461fe24da
Sebastian <89323242+sebastianelbacho@users.noreply.github.com>
2022-12-25 14:16:21 +0000

    Update javaScript.js

--- pizza project/pizza_JavaScript/javaScript.js
+++ pizza project/pizza_JavaScript/javaScript.js
@@ -30,3 +30,32 @@ function getReceipt() {
     // these variables will get passed on to each function
     getTopping(runningTotal, text1);
 };
+
+function getTopping(runningTotal, text1) {
+    var toppingTotal = 0;
+    var selectedTopping = [];
+    var toppingArray = document.getElementsByClassName("toppings");
+    for (var j = 0; j < toppingArray.length; j++) {
+        if (toppingArray[j].checked) {
+            selectedTopping.push(toppingArray[j].value);
+            console.log("selected topping item: (" + toppingArray[j].value + ")");
+            text1 = text1 + toppingArray[j].value + "<br>";
+        }
+    }
+
+    var toppingCount = selectedTopping.length;
+    if (toppingCount > 1) {
+        toppingTotal = (toppingCount - 1);
+    } else {
+        toppingTotal = 0;
+    }
+
+    runningTotal = (runningTotal + toppingTotal);
+    console.log("total selected topping items: " + toppingCount);
+    console.log(toppingCount + " topping - 1 free topping = " + "$" + toppingTotal + ".00");
+    console.log("topping text1: " + text1);
+    console.log("Purchase Total: " + "$" + runningTotal + ".00");
+    document.getElementById("showText").innerHTML = text1;
+    document.getElementById("totalPrice").innerHTML = "</h3>Total: <strong>$" +
+        runningTotal + ".00" + "</strong><h3>";
+};
